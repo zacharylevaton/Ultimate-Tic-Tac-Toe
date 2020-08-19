59ee3355e6b76bf2d29674780076f2c35dc77e1c
Zach Levaton <zach.levaton.0514@gmail.com>
2020-08-19 15:41:29 -0700

    Re-organized code to make content updating its own function. Updated all dimensions to be responsive to the set window height.

--- make_board.py
+++ make_board.py
@@ -3,12 +3,14 @@ pygame.font.init()
 
 
 # Colors
+black 			= (   0,   0,   0)
 white  			= ( 255, 255, 255)
-gray  			= ( 154, 162,  151)
+gray  			= ( 154, 162, 151)
+dark_gray		= ( 123, 130, 121)
 
 # Board/space dimensions
 total_height	= 720
-total_width		= 900
+total_width		= int(total_height * 1.25)
 board			= total_height
 sidebar_width 	= total_width - board
 
@@ -18,8 +20,10 @@ x_score			= 0
 o_score			= 0
 
 # Initializing font-related info
-title_font 		= pygame.font.SysFont('arial', 25)
-content_font 	= pygame.font.SysFont('arial', 50)
+title_size 		= total_height // 30
+content_size 	= title_size * 2
+title_font 		= pygame.font.SysFont('arial', title_size)
+content_font 	= pygame.font.SysFont('arial', content_size)
 
 """ 
 Setting board elements' dimensions based on board size
@@ -40,60 +44,64 @@ def make_board(window):
 	x = local_margin
 	y = local_margin
 	for row in range(1,10):
-		if row  == 4 or row == 7:
+		if row  in [4, 7]:
 			y += global_margin + space
 		elif row != 1:
 			y += local_margin+ space
 
 		for column in range(1,10):
-			if column  == 4 or column == 7:
+			if column  in [4, 7]:
 				x += global_margin + space
 			elif column != 1:
 				x += local_margin+ space
+
+			if row in [1, 4, 7] and column in [1, 4, 7]:
+				pygame.draw.rect(window, dark_gray, (x,y,3*space + 2*local_margin,3*space + 2*local_margin))
 
 			pygame.draw.rect(window, white, (x,y,space,space))
 
 			if column == 9:
 				x = local_margin
 
-
-def update_sidebar(window, current_player, x_score, o_score):
 	# Creating text for current player title block.
 	current_player_title = title_font.render('Current Player:', True, white) 
 	current_player_title_rect = current_player_title.get_rect()  
 	current_player_title_rect.center = (total_width - (0.5 * sidebar_width), local_margin + 0.5 * space)
-
-	# Updating text content for current player.
-	current_player_content = content_font.render(current_player, True, white) 
-	current_player_content_rect = current_player_content.get_rect()  
-	current_player_content_rect.center = (total_width - (0.5 * sidebar_width), 2 * local_margin + 1.5 * space)
 
 	# Creating text for x score title block.
 	x_score_title		= title_font.render('X Wins:', True, white) 
 	x_score_title_rect = x_score_title.get_rect()
 	x_score_title_rect.center = (total_width - (0.5 * sidebar_width), 3 * local_margin + 1 * global_margin + 3.5 * space)
 
-	# Updating text content for x score.
-	x_score_content		= content_font.render(str(x_score), True, white) 
-	x_score_content_rect = x_score_content.get_rect()
-	x_score_content_rect.center = (total_width - (0.5 * sidebar_width), 4 * local_margin + 1 * global_margin + 4.5 * space)
-
 	# Creating text for o score title block.
 	o_score_title = title_font.render('O Wins:', True, white) 
 	o_score_title_rect = o_score_title.get_rect()
 	o_score_title_rect.center = (total_width - (0.5 * sidebar_width), 5 * local_margin + 2 * global_margin + 6.5 * space)
+
+	# Blitting titles to screen.
+	window.blit(current_player_title, current_player_title_rect)
+	window.blit(x_score_title, x_score_title_rect)
+	window.blit(o_score_title, o_score_title_rect)
+
+def update_sidebar(window, current_player, x_score, o_score):
+	# Updating text content for current player.
+	current_player_content = content_font.render(current_player, True, white) 
+	current_player_content_rect = current_player_content.get_rect()  
+	current_player_content_rect.center = (total_width - (0.5 * sidebar_width), 2 * local_margin + 1.5 * space)
+
+	# Updating text content for x score.
+	x_score_content		= content_font.render(str(x_score), True, white) 
+	x_score_content_rect = x_score_content.get_rect()
+	x_score_content_rect.center = (total_width - (0.5 * sidebar_width), 4 * local_margin + 1 * global_margin + 4.5 * space)
 
 	# Updating text content for o score.
 	o_score_content		= content_font.render(str(o_score), True, white) 
 	o_score_content_rect = o_score_content.get_rect()
 	o_score_content_rect.center = (total_width - (0.5 * sidebar_width), 6 * local_margin + 2 * global_margin + 7.5 * space)
 
-	# Blitting all text to screen
-	window.blit(current_player_title, current_player_title_rect)
+	# Blitting content to screen.
 	window.blit(current_player_content, current_player_content_rect)
-	window.blit(x_score_title, x_score_title_rect)
 	window.blit(x_score_content, x_score_content_rect)
-	window.blit(o_score_title, o_score_title_rect)
 	window.blit(o_score_content, o_score_content_rect)
 
 
