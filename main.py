47719655c3b156bb0c4073429dd8daba12c52bf9
Zach Levaton <zach.levaton.0514@gmail.com>
2020-08-19 15:22:44 -0700

    Minor edits.

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from make_board import *
 pygame.init()
 
 
-window 	= pygame.display.set_mode((total_board_width, total_board_height))
+window 	= pygame.display.set_mode((total_width, total_height))
 pygame.display.set_caption("Ultimate Tic-Tac-Toe")
 
-make_board(window)
+refresh_window(window, current_player, x_score, o_score)
 
 while True:
 	pygame.time.delay(100)
