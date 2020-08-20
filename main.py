6914c0a27bca05500f85e9c27dbd77a8c996f034
Zach Levaton <zach.levaton.0514@gmail.com>
2020-08-19 18:53:28 -0700

    Added in button calls for in-game function

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from make_board import *
 pygame.init()
 
 
-def reset_game():
-	window 	= pygame.display.set_mode((total_width, total_height))
-	pygame.display.set_caption("Ultimate Tic-Tac-Toe")
-	refresh_window(window, current_player, x_score, o_score)
-	run_game()
-
-
 def quit_game():
 	pygame.quit()
 	sys.exit(0)
 
+def reset_game():
+	# Reset Here
+	pass
 
 def run_game():
+	window 	= pygame.display.set_mode((total_width, total_height))
+	pygame.display.set_caption("Ultimate Tic-Tac-Toe")
+	refresh_window(window, current_player, x_score, o_score)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit_game()
 
-		#button(window, "Reset", 150,450,100,50, white,black,)
+		b_height 	= sidebar_width // 2
+		b_width 	= sidebar_width // 3
+		b_x			= board
+		b_y 		= total_height - sidebar_width // 3
+		button(window, "Reset", b_x, b_y, b_height, b_width, yellow, light_yellow, reset_game)
+		button(window, "Quit", b_x + sidebar_width // 2, b_y, b_height, b_width, red, light_red, quit_game)
 
 		pygame.display.update()
 
 if __name__ == "__main__":
-	reset_game()
+	run_game()
