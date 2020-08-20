d4d377834d1ee49680625e71ab0e3cded1d0493f
Zach Levaton <zach.levaton.0514@gmail.com>
2020-08-19 18:26:26 -0700

    Organized functions

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import pygame 
+import pygame, sys
 from make_board import *
 pygame.init()
 
 
-window 	= pygame.display.set_mode((total_width, total_height))
-pygame.display.set_caption("Ultimate Tic-Tac-Toe")
+def reset_game():
+	window 	= pygame.display.set_mode((total_width, total_height))
+	pygame.display.set_caption("Ultimate Tic-Tac-Toe")
+	refresh_window(window, current_player, x_score, o_score)
+	run_game()
 
-refresh_window(window, current_player, x_score, o_score)
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit(0)
+def quit_game():
+	pygame.quit()
+	sys.exit(0)
 
-	pygame.display.update()
+
+def run_game():
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit_game()
+
+		#button(window, "Reset", 150,450,100,50, white,black,)
+
+		pygame.display.update()
+
+if __name__ == "__main__":
+	reset_game()
